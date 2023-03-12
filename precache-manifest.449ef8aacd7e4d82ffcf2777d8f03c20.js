3e338ecf58314b9b7069ad78957ded2db911384c
ANIL ACAR <acar@ANILs-MacBook-Air.local>
2023-03-13 00:00:32 +0100

    Updates

---
+++ precache-manifest.449ef8aacd7e4d82ffcf2777d8f03c20.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224238dd77ddd61a775da1fab7525012",
+    "url": "/sticky-notes/index.html"
+  },
+  {
+    "revision": "6c2890b3b64f78542b86",
+    "url": "/sticky-notes/static/js/2.2d8ad4ed.chunk.js"
+  },
+  {
+    "revision": "6aa8b6da2e70f12b0d9f",
+    "url": "/sticky-notes/static/js/main.7549370c.chunk.js"
+  },
+  {
+    "revision": "c8d1d7f626986c938602",
+    "url": "/sticky-notes/static/js/runtime-main.31ad1808.js"
+  }
+]);
